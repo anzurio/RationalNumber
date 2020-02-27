97cd6184ae4b9b39ac2036a11c28a670a878c2e6
mario.parra <anzurio@gmail.com>
2020-02-27 13:47:22 -0600

    Add stub project files

---
+++ Anzurio.Rational.Tests/RationalNumberConstruction.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anzurio.Rational.Tests
+{
+    [TestFixture]
+    public class RationalNumberConstruction
+    {
+        [TestCase]
+        public void ConstructRationalNumberFromWholeNumber(int wholeNumber)
+        {
+
+        }
+    }
+}
---
+++ Anzurio.Rational/RationalNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anzurio.Rational
+{
+    public class RationalNumber
+    {
+        internal int Numerator { get; set; }
+        internal int Denominator { get; set; }
+
+        public RationalNumber(int whole, int numerator, int denominator)
+        {
+
+        }
+
+        public RationalNumber(int numerator, int denominator)
+        {
+
+        }
+
+        public RationalNumber(int whole)
+        {
+
+        }
+    }
+}
